ab9a2c3521c1f53d0f4fd248448e34e55e761d39
toyman640 <falakoadebayor@gmail.com>
2023-08-24 19:27:46 +0100

    Fix linters error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :post, class_name: 'Post', foreign_key: :post_id
 
   after_create :update_post_comments_counter
-  
+
   def update_post_comments_counter
     post.update(comments_counter: post.comments.count)
   end
