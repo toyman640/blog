a0f94ba265ffb22edb509231e028264a6a090d3f
toyman640 <falakoadebayor@gmail.com>
2023-08-24 14:45:19 +0100

    Add class methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
   belongs_to :author_id, class_name: 'User', foreign_key: :author_id_id
   has_many :comments
   has_many :likes
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def update_comments_counter
+    update(comments_counter: comments.count)
+  end
+
+  def update_likes_counter
+    update(likes_counter: likes.count)
+  end
 end
