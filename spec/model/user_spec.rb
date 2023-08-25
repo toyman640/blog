da44cb6e6b0940ea50fb8e43bbb79890074ef0dd
toyman640 <falakoadebayor@gmail.com>
2023-08-25 18:15:27 +0100

    Fix linters error

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')}
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
 
   before { subject.save }
   it 'name should be present' do
@@ -13,5 +13,4 @@ RSpec.describe User, type: :model do
     subject.posts_counter = 'invalid'
     expect(subject).to_not be_valid
   end
-
 end
