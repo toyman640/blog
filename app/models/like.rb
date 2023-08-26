981d1fcbbbb756e579abb013b728ef6354e2e50d
toyman640 <falakoadebayor@gmail.com>
2023-08-26 15:40:50 +0100

    retry test

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
   belongs_to :post, class_name: 'Post'
-  belongs_to :author_id, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
 end
--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  subject { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
 
   it 'title should be present' do
-    post = Post.new(author: subject.id, title: 'Hello', text: 'This is my first post')
+    post = Post.new(author: subject, title: 'Hello', text: 'This is my first post')
     post.title = nil
     expect(post).to_not be_valid
   end
@@ -23,7 +23,7 @@ RSpec.describe Post, type: :model do
   end
 
   it 'title should not be more than 250 characters' do
-    post = Post.new(author: subject.id, title: 'A' * 251, text: 'This is my first post')
+    post = Post.new(author: subject, title: 'A' * 251, text: 'This is my first post')
     expect(post).to_not be_valid
   end
 
@@ -39,4 +39,47 @@ RSpec.describe Post, type: :model do
     post.comments_counter = 0
     expect(post).to be_valid
   end
+
+  it 'returns the five most recent comments' do
+    user = User.create(name: 'Billy', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Nile.')
+    post = Post.create(author: user, title: 'Hello', text: 'This is my first post')
+
+    old_comments = []
+    3.times { old_comments << Comment.create(post: post.id, author: user.id) }
+    
+    new_comments = []
+    5.times { new_comments << Comment.create(post: post.id, author: user.id) }
+
+    # Print out the comments associated with the post for debugging
+    puts "Old Comments: #{post.comments.inspect}"
+    puts "New Comments: #{new_comments.inspect}"
+
+    recent_comments = post.recent_comments
+
+    expect(recent_comments).to eq(new_comments.reverse)
+  end
+  # ============================
+  it 'updates comments_counter when comments are added' do
+    post = Post.create(author: subject, title: 'Hello', text: 'This is my first post')
+
+    expect {
+      Comment.create(post: post, author: subject)
+    }.to change { post.comments_counter }.by(1)
+  end
+
+  it 'updates likes_counter when likes are added' do
+    post = Post.create(author: subject, title: 'Hello', text: 'This is my first post')
+
+    expect {
+      Like.create(post: post, author: subject)
+    }.to change { post.likes_counter }.by(1)
+  end
+
+  it 'increments user posts_counter after post creation' do
+    expect {
+      Post.create(author: subject, title: 'Hello', text: 'This is my first post')
+      subject.reload
+    }.to change { subject.posts_counter }.by(1)
+  end
+
 end
