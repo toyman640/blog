86b40dd709cb806c13ff000345f1d1229132052f
toyman640 <falakoadebayor@gmail.com>
2023-08-27 13:56:56 +0100

    Implement likes counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,11 @@
 class Like < ApplicationRecord
-  belongs_to :post, class_name: 'Post', counter_cache: true
+  belongs_to :post, class_name: 'Post'
   belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+  after_create :update_post_likes_counter
+
+  def update_post_likes_counter
+    post.update(likes_counter: post.likes.count)
+    reload
+  end
 end
