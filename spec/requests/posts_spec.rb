5256a6589b6ca670e3bdba7f60fea01960cdf829
toyman640 <falakoadebayor@gmail.com>
2023-08-29 17:06:07 +0100

    Fix linters error

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,22 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /users/:id/posts" do
-    
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /users/:id/posts' do
     let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
-    it "returns success http status code" do
+    it 'returns success http status code' do
       get "/users/#{user.id}/posts"
       expect(response.status).to eq(200)
     end
 
-    it "returns correct template" do
+    it 'returns correct template' do
       get "/users/#{user.id}/posts"
-      expect(response).to render_template("posts/index")
+      expect(response).to render_template('posts/index')
     end
 
-    it "returns body that include placeholder text" do
+    it 'returns body that include placeholder text' do
       get "/users/#{user.id}/posts"
-      expect(response.body).to include("Number of posts: x")
+      expect(response.body).to include('Number of posts: x')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "it returns success http status code" do
-      get "/users"
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
+    it 'it returns success http status code' do
+      get '/users'
       expect(response.status).to eq(200)
     end
 
-    it "returns the correct template" do
-      get "/users"
-      expect(response).to render_template("users/index")
+    it 'returns the correct template' do
+      get '/users'
+      expect(response).to render_template('users/index')
     end
 
-    it "returns body that include placeholder text" do
-      get "/users"
-      expect(response.body).to include("All Users")
+    it 'returns body that include placeholder text' do
+      get '/users'
+      expect(response.body).to include('All Users')
     end
   end
 end
