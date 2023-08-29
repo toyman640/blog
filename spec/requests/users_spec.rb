0a0adfb61d7d80167380646114100105469d9b18
toyman640 <falakoadebayor@gmail.com>
2023-08-29 20:02:12 +0100

    Add test for users show method

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /users' do
+    let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+
     it 'it returns success http status code' do
       get '/users'
       expect(response.status).to eq(200)
@@ -15,6 +17,13 @@ RSpec.describe 'Users', type: :request do
     it 'returns body that include placeholder text' do
       get '/users'
       expect(response.body).to include('All Users')
+    end
+
+    it 'returns specific user when id is supplied' do
+      get "/users/#{user.id}"
+
+      expect(response.status).to eq(200)
+      expect(user.name).to eq('Tom')
     end
   end
 end
