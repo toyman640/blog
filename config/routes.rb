b766dbc614d047a7b38250de9d3cdf9b52040b7d
toyman640 <falakoadebayor@gmail.com>
2023-08-30 16:21:09 +0100

    Link all pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
   get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  get '/users/:id/posts', to: 'posts#index'
+  get '/users/:id', to: 'users#show', as: "user"
+  get '/users/:id/posts', to: 'posts#index', as: "post"
   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
 
 end
