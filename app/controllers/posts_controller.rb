8b73fe63473e16f8609c85523c1a476929efe385
toyman640 <falakoadebayor@gmail.com>
2023-08-31 10:03:09 +0100

    Add recent_comment method to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,12 @@ class PostsController < ApplicationController
   def index
     @author = User.find(params[:id])
     @author_posts = Post.where(author_id: @author.id)
+    @post_comments = []
+    @author_posts.each do |post|
+      @post_comments.concat(post.recent_comments)
+    end
   end
+  
 
   def show
     @specific_user = User.find(params[:user_id])
