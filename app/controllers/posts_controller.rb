49ff10d161c56a7aa695fb6dea657c2987bc9f7d
toyman640 <falakoadebayor@gmail.com>
2023-08-31 18:57:41 +0100

    Add form for current_user to create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,27 @@ class PostsController < ApplicationController
     @specific_post = Post.where(author_id: @specific_user.id).find(params[:id])
     @post_comments = Comment.where(post_id: @specific_post.id)
   end
+
+  def new
+    @new_post = Post.new
+  end
+  
+  def create
+    @new_post = Post.new(post_params)
+    @new_post.author_id = current_user.id
+  
+    if @new_post.save
+      redirect_to users_path, notice: 'Post was successfully created.'
+    else
+      flash.now[:alert] = 'Failed to create the post.'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
-  get '/users', to: 'users#index'
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/new-post', to: 'posts#new', as: 'new_post'
+  post '/users/new-post-create', to: 'posts#create', as: 'create_post'
   get '/users/:id', to: 'users#show', as: "user"
   get '/users/:id/posts', to: 'posts#index', as: "post"
   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-
+  
 end
