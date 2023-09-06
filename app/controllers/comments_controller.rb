01801e584bd4e7e560f1b6b8af1ce70e24d57d6f
toyman640 <falakoadebayor@gmail.com>
2023-09-06 16:07:26 +0100

    Make required change

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
     @specific_post = Post.find(params[:post_id])
     @specific_user = User.find(params[:user_id])
     @comment = Comment.new(
-      author_id: @specific_user.id,
+      author_id: current_user.id,
       post_id: @specific_post.id,
       text: params[:text]
     )
